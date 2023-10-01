48b2158214e5c45ac49f248255054c50fe468813
Wave Demure <144980384+WaveDemure@users.noreply.github.com>
2023-10-01 15:26:28 -0500

    How did a miss that

--- index.js
+++ index.js
@@ -13,10 +13,10 @@ function openGame() {
 function openHome() {
     var page = new ABC({
         "type": "blank",
-        "url": atob("aHR0cHM6Ly9lZHVjYXRpb24uZnIudG8=")
+        "url": atob("aHR0cHM6Ly9wcm9qZWN0aGl2ZS5mci50by8=")
     })
     page.setType("blank")
-    page.setUrl(atob("aHR0cDovL3Byb2plY3RoaXZlLmZyLnRvLw=="))
+    page.setUrl(atob("aHR0cHM6Ly9wcm9qZWN0aGl2ZS5mci50by8="))
     console.log(page.getCode())
     page.open()
     window.close
