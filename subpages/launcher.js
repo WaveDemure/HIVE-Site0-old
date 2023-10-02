a2b8a285f7fdc92e63c39b53f7018c45b37ba05c
Wave Demure <144980384+WaveDemure@users.noreply.github.com>
2023-10-01 20:00:46 -0500

    Update launcher.js

--- subpages/launcher.js
+++ subpages/launcher.js
@@ -1,6 +1,7 @@
 function home() {
-    location.href = "https://projecthive.ddns.net"
+    location.href = "https://projecthive.fr.to"
 }
+
 function open() {
     //Creates new ABC
     var page = new ABC({
